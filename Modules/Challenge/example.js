369f054ab6a17bde1ac70235bc7b86b2c508c116
Mitch <mitch@Mitchs-Mac-Pro.attlocal.net>
2023-10-09 21:42:23 -0400

    For PG

---
+++ Modules/Challenge/example.js
@@ -0,0 +1,14 @@
+let age = 44;
+console.log(age);
+
+let isStudent = true;
+console.log(isStudent);
+
+let favoriteColor = ['green','yellow','black'];
+console.log(favoriteColor);
+
+let person = {
+    name:'Anthony',
+    age: 44,
+    isStudent
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Js-Code-Challenge
+
+This is code challenge repository for Js
